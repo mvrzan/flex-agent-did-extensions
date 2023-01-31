5b82bc9a7b8ab4153915e4127c7331b1259b0297
Matija Vrzan <mvrzan@icloud.com>
2023-01-30 21:22:04 -0500

    Feature/twilio assets (#8)
    * adding twilio assets usage
    
    * adding twilio assets
    
    * adding twilio assets
    
    * function clean up
    
    * instantQuery clean up

---
+++ agent-did-extensions/src/utils/instantQuerySearch/instantQuerySearch.js
@@ -0,0 +1,26 @@
+import { Manager } from '@twilio/flex-ui';
+
+/**
+ * Promise wrapper for instantQuery search. Enables the caller
+ * to await the search results instead of configuring callbacks
+ * and query result listeners.
+ * @param index instant query index to search
+ * @param query query expression
+ * @returns Promise of query results
+ */
+const instantQuerySearch = async (index, query) => {
+  const instantQueryClient =
+    await Manager.getInstance().insightsClient.instantQuery(index);
+
+  const queryPromise = new Promise(resolve => {
+    instantQueryClient.on('searchResult', items => {
+      resolve(items);
+    });
+  });
+
+  await instantQueryClient.search(query);
+
+  return queryPromise;
+};
+
+export default instantQuerySearch;
